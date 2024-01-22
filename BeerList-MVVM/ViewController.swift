ef7bff6eae0bde9295a9a06c975e55f944858a45
Vladyslav <vladyslavnh01@gmail.com>
2024-01-22 11:21:50 +0200

    Initial Commit

---
+++ BeerList-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerList-MVVM
+//
+//  Created by MacBook on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
